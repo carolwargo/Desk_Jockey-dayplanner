6f915af10df28eef3e0dbddeae130add00a459e5
Carol Wargo <84477950+carolwargo@users.noreply.github.com>
2023-03-23 22:06:12 -0400

    Update script

--- script.js
+++ script.js
@@ -1,80 +1,62 @@
-var nine=$("#9");
-var ten=$("#10");
-var eleven=$("11");
-var twelve=$("12");
-var one=$("13");
-var two=$("14");
-var three=$("15");
-var four=$("16");
-var five=$("17");
-var hour=moment().hour();
+body {
+    font-family: 'Open Sans', sans-serif;
+    font-size: 16px;
+    width: 50%;
+  }
+  
+  textarea {
+    background: transparent;
+    border: none;
+    resize: none;
+    color: #000000;
+    border-left: 1px solid black;
+    padding: 10px;
+  }
+  
+  .description {
+    white-space: pre-wrap;
 
-const currentDate = dayjs().format("dddd, MMMM D");
-const currentDayElement = document.getElementById("currentDay");
-currentDayElement.textContent = currentDate;
-
-
-var date=setInterval(function(){
-    var currentDate = moment().format('dddd')+"," + moment().format("MMMM Do YYYY");
-    $('#currentDay').text(currentDate);
-}, 
-1000); 
-
-function colorStyle(){
-    $("textarea").each(function(){
-
-   var time=parseInt($(this).attr('id'))
-    
-        hour=parseInt(hour)
-        if (hour < time) {
-        $(this).addClass("past")
-        } 
-    
-        else if (hour ===time) {
-        $(this).addClass("present")
-        } 
-
-        else {
-        $(this).addClass("future")  
-        }
-
-  })
-}
-
-//LOCAL STORAGE JSON//
-function store(){
-    var nineAm = JSON.parse(localStorage.getItem("9AM"))
-    var tenAm = JSON.parse(localStorage.getItem("10AM"))
-    var elevenAm = JSON.parse(localStorage.getItem("11AM"))
-    var twelvePm = JSON.parse(localStorage.getItem("12PM"))
-    var onePm = JSON.parse(localStorage.getItem("1PM"))
-    var twoPm = JSON.parse(localStorage.getItem("2PM"))
-    var threePm = JSON.parse(localStorage.getItem("3PM"))
-    var fourPm = JSON.parse(localStorage.getItem("4PM"))
-    var fivePm = JSON.parse(localStorage.getItem("5PM"))
-
-    
-    nine.val(nineAm)
-    ten.val(tenAm)
-    eleven.val(elevenAm)
-    twelve.val(twelvePm)
-    one.val(onePm)
-    two.val(twoPm)
-    three.val(threePm)
-    four.val(fourPm)
-    five.val(fivePM)
-
-}
-
-//Set up save button//
-$(document).ready(function(){
-    $("#saveBtn").on("click",function(){
-        textinput = $(this).siblings("textarea").val().trim();
-        hours = $(this).siblings(".input-group-prepend").text().trim();
-        localStorage.setItem(hours, JSON.stringify(textinput));
-    })
-
-    store();
-    colorStyle();
-})
-
+  }
+  
+  .time-block {
+    border-radius: 15px;
+  }
+  
+  .row {
+    border-top: 1px solid white;
+  }
+  
+  .hour {
+    background-color: #ffffff;
+    color: #000000;
+    border-top: 1px dashed #000000;
+  }
+  
+  .past {
+    background-color: #d3d3d3 ;
+    color: white;
+  }
+  
+  .present {
+    background-color: #ff6961;
+    color: white;
+  }
+  
+  .future {
+    background-color: #77dd77;
+    color: white;
+  }
+  
+  .saveBtn {
+    border-left: 1px solid black;
+    border-top-right-radius: 15px;
+    border-bottom-right-radius: 15px;
+    background-color: #06aed5;
+    color: white;
+  }
+  
+  .saveBtn i:hover {
+    font-size: 20px;
+    transition: all 0.3s ease-in-out;
+  }
+  
